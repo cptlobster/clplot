afc71ffab45a64247208e105fcc38dbd23671fa0
Dustin Thomas <stdio@cptlobster.dev>
2024-09-30 14:35:01 -0500

    start adding other CLI args for when we add actual graphing tools

--- src/main.rs
+++ src/main.rs
@@ -22,57 +22,89 @@ use crate::renderer::plot::Plot;
 use crate::data::PVec2;
 use crate::renderer::shapes::Line;
 use crate::renderer::terminal::get_size;
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use clio::Input;
 
 /// Command-line graphing and plotting utility.
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// Input data. Defaults to stdin.
+    #[arg(short, long, value_parser, default_value="-")]
+    input_file: Input,
+
+    /// Format of data input.
+    #[arg(short, long, default_value="float")]
+    format: String,
+
     /// Width of draw area. If undefined, uses full width of console.
-    #[arg(long, default_value_t = 0)]
+    #[arg(short='W', long, default_value_t = 0)]
     width: u16,
 
     /// Height of draw area. If undefined, uses full height of console.
-    #[arg(long, default_value_t = 0)]
+    #[arg(short='H', long, default_value_t = 0)]
     height: u16,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Display test output
+    Test {},
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
+
+    // set plot width and height
     let size: (u16, u16) = get_size();
-    let width: u16 = size.0;
-    let height: u16 = size.1 - 1;
+    let width: u16;
+    if (args.width != 0) { width = args.width; }
+    else { width = size.0; }
+    let height: u16;
+    if (args.height != 0) { height = args.height; }
+    else { height = size.1 - 1; }
+
+    // create and clear the plot area
     let plot: Plot = Plot::new(width, height);
     plot.clear();
-    plot.put('b', &PVec2::new(11, 5));
-    plot.put('e', &PVec2::new(20, 12));
-    plot.put('a', &PVec2::new(32, 7));
-    plot.put('n', &PVec2::new(45, 20));
-    plot.put('s', &PVec2::new(69, 22));
-    plot.put('.', &PVec2::new(80, 17));
-    plot.put('.', &PVec2::new(92, 25));
-    plot.put('.', &PVec2::new(110, 29));
-    plot.put_str("ha! I love printing!", &PVec2::new(3, 1));
-    plot.put_str("what if I have...\na newline?", &plot.origin_bl(3, 4));
-    plot.put_str("AAAA\nAAAA\nAAAA\nAAAA", &PVec2::new(3, 7));
-    plot.put_str_transparent("B  B\nBB  \n  BB\n B B", &PVec2::new(3, 7));
-    let l1 = Line::new(PVec2::new(1, 1), plot.origin_bl(1, 1), '|');
-    let l2 = Line::new(plot.origin_bl(1, 1), plot.origin_br(1, 1), '-');
-    l1.draw(&plot);
-    l2.draw(&plot);
-    plot.clear();
-    plot.put_str("this should be different...", &PVec2::new(3, 1));
-    plot.put_str("what if I have...\na newline?", &plot.origin_bl(3, 4));
-    plot.put_str("AAAA\nAAAA\nAAAA\nAAAA", &PVec2::new(3, 7));
-    plot.put_str_transparent("B  B\nBB  \n  BB\n B B", &PVec2::new(3, 7));
-    let l1 = Line::new(PVec2::new(1, 1), plot.origin_bl(1, 1), '|');
-    let l2 = Line::new(plot.origin_bl(1, 1), plot.origin_br(1, 1), '-');
-    l1.draw(&plot);
-    l2.draw(&plot);
-    let l3 = Line::new(PVec2::new(2, 2), plot.origin_br(4, 2), '#');
-    let l4 = Line::new(plot.origin_bl(2, 2), plot.origin_br(2, 10), '#');
-    l3.draw(&plot);
-    l4.draw(&plot);
-    plot.finish();
-    Ok(())
+
+    match &args.command {
+        Commands::Test{} => {
+            // print all our test characters
+            plot.put('b', &PVec2::new(11, 5));
+            plot.put('e', &PVec2::new(20, 12));
+            plot.put('a', &PVec2::new(32, 7));
+            plot.put('n', &PVec2::new(45, 20));
+            plot.put('s', &PVec2::new(69, 22));
+            plot.put('.', &PVec2::new(80, 17));
+            plot.put('.', &PVec2::new(92, 25));
+            plot.put('.', &PVec2::new(110, 29));
+            plot.put_str("ha! I love printing!", &PVec2::new(3, 1));
+            plot.put_str("what if I have...\na newline?", &plot.origin_bl(3, 4));
+            plot.put_str("AAAA\nAAAA\nAAAA\nAAAA", &PVec2::new(3, 7));
+            plot.put_str_transparent("B  B\nBB  \n  BB\n B B", &PVec2::new(3, 7));
+            let l1 = Line::new(PVec2::new(1, 1), plot.origin_bl(1, 1), '|');
+            let l2 = Line::new(plot.origin_bl(1, 1), plot.origin_br(1, 1), '-');
+            l1.draw(&plot);
+            l2.draw(&plot);
+            plot.clear();
+            plot.put_str("this should be different...", &PVec2::new(3, 1));
+            plot.put_str("what if I have...\na newline?", &plot.origin_bl(3, 4));
+            plot.put_str("AAAA\nAAAA\nAAAA\nAAAA", &PVec2::new(3, 7));
+            plot.put_str_transparent("B  B\nBB  \n  BB\n B B", &PVec2::new(3, 7));
+            let l1 = Line::new(PVec2::new(1, 1), plot.origin_bl(1, 1), '|');
+            let l2 = Line::new(plot.origin_bl(1, 1), plot.origin_br(1, 1), '-');
+            l1.draw(&plot);
+            l2.draw(&plot);
+            let l3 = Line::new(PVec2::new(2, 2), plot.origin_br(4, 2), '#');
+            let l4 = Line::new(plot.origin_bl(2, 2), plot.origin_br(2, 10), '#');
+            l3.draw(&plot);
+            l4.draw(&plot);
+            plot.finish();
+            Ok(())
+        }
+    }
 }
